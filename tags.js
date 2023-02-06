f84ae4f9e2716554d86f8f9de9a8d5fe606f1e4e
Albert-FFS <r.a.s.m.13@hotmail.com>
2023-02-05 17:50:16 -0800

    Acambaro elige primera pagina

---
+++ tags.js
@@ -0,0 +1,10 @@
+const metaSeo = `
+<meta name="title" content="Acambaro Elige | Home">
+<meta name="description" content="Bienvenidos a Acámbaro Elige con las noticias políticas de la región">
+<meta name="keywords" content="Acámbaro, Guanajuato, Bajío, Política, 417, Acambarense, México">
+<meta name="robots" content="index, follow">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="language" content="Spanish">
+<meta name="revisit-after" content="7 days">
+<meta name="author" content="Albert-FFS">
+`;
